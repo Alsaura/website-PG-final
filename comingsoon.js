f813ea9f645e17ab0e75b67cec89164fa88cfe03
Alsaura <103641448+Alsaura@users.noreply.github.com>
2023-10-06 03:07:06 +0700

    Add files via upload

---
+++ comingsoon.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let images = document.querySelectorAll(".content2 img");
+    let content2Position = document.querySelector(".content2").offsetTop;
+    let windowHeight = window.innerHeight;
+
+    function debounce(func, wait = 10, immediate = true) {
+        let timeout;
+        return function() {
+            let context = this, args = arguments;
+            let later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            let callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+    function showImages() {
+        let scrollPosition = window.scrollY;
+
+        if (scrollPosition + windowHeight >= content2Position) {
+            images.forEach((image, index) => {
+                setTimeout(() => {
+                    image.classList.add('show');
+                }, index * 150);
+            });
+
+
+            window.removeEventListener('scroll', debouncedShowImages);
+        } else {
+            images.forEach(image => {
+                image.classList.remove('show');
+            });
+        }
+    }
+
+    let debouncedShowImages = debounce(showImages, 10);
+
+    window.addEventListener('scroll', debouncedShowImages);
+});
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+
+    setTimeout(() => {
+        navLinks.classList.remove('show');
+    }, 5000);
+});
+
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Ganti gambar setiap 4 detik
+}
+
+showSlides();
+
